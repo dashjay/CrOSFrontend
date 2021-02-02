f3ade8556218fc3bdd43b0faa5a1fcb41c9b68ed
DashJay <dashjay@aliyun.com>
2021-02-02 09:54:55 +0800

    feat(UserInfo): 添加一个操作菜单，方便功能扩展

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <!-- HEAD: 用户信息card -->
     <card :header="{ title: '用户信息' }">
       <p slot="content" class="card-padding">
         <cell title="用户名:" :value="Name"></cell>
       </p>
     </card>
 
+    <!-- HEAD: 功能按钮区 -->
     <flexbox
       class="flex-container cover-button"
       style="padding-top: 10px; padding-bottom: 20px"
@@ -22,6 +24,7 @@
       </flexbox-item>
     </flexbox>
 
+    <!-- HEAD: 已定教室列表 -->
     <card :header="{ title: '已定教室' }">
       <div slot="content" class="card-flex card-demo-content01">
         <div class="vux-1px-r">
@@ -60,20 +63,29 @@
 
         <div class="vux-1px-r">
           <br />
-          <span>{{ status_detail[item.status] }}</span>
+          <span :style="'color: ' + status_color[item.status]">{{
+            status_detail[item.status]
+          }}</span>
         </div>
 
         <div class="vux-1px-r" style="padding-top: 10px">
           <x-button
-            :type="ButtonColor(item.status)"
-            style="width: 70px; height: 30px; color: black; font-size: 10px"
+            type="primary"
+            style="width: 70px; height: 30px; color: white; font-size: 13px"
             :data-record_id="item.id"
-            @click.native="refund(item.id, item.status)"
-            >{{ ButtonText(item.status) }}
+            @click.native="clickButton(item)"
+            >操作
           </x-button>
         </div>
       </div>
     </card>
+
+    <actionsheet
+      v-model="show_op_menu"
+      :menus="menu"
+      theme="android"
+      @on-click-menu="clickmenu"
+    ></actionsheet>
 
     <div>
       <confirm
@@ -90,17 +102,62 @@
         <p style="text-align: center">{{ "请输入退订原因，并点击确定" }}</p>
       </confirm>
     </div>
+
+    <popup v-model="showqr" position="left" width="100%">
+      <div class="qr">
+        <span class="qrcode">
+          <qrcode :value="qrsource" type="img"></qrcode>
+        </span>
+        <x-button
+          type="primary"
+          style="color: white; width: 70%"
+          @click.native="showqr = false"
+          >点击返回</x-button
+        >
+      </div>
+    </popup>
   </div>
 </template>
 
 <script>
-import { Cell, Card, FormPreview, Flexbox, FlexboxItem } from "vux";
+import {
+  Cell,
+  Card,
+  FormPreview,
+  Flexbox,
+  FlexboxItem,
+  Popup,
+  Actionsheet,
+  Qrcode,
+} from "vux";
+
 import { mapGetters, mapState } from "vuex";
 
 export default {
   name: "UserInfo.vue",
+  components: {
+    Cell,
+    Card,
+    FormPreview,
+    Flexbox,
+    FlexboxItem,
+    Popup,
+    Actionsheet,
+    Qrcode,
+  },
   data() {
     return {
+      // 操作菜单
+      show_op_menu: false,
+      menu: {
+        cancel: "取消",
+        showqr: "显示二维码",
+        remind: "提醒审核",
+      },
+      current_clicked_booking: null,
+      qrsource: "null",
+
+      showqr: false,
       classroomId: "",
       showRefund: false,
       Lending: true,
@@ -111,15 +168,9 @@ export default {
   created() {
     this.refreshList();
   },
-  components: {
-    Cell,
-    Card,
-    FormPreview,
-    Flexbox,
-    FlexboxItem,
-  },
+
   computed: {
-    ...mapState(["status", "status_detail"]),
+    ...mapState(["status", "status_detail", "status_color"]),
     ...mapGetters(["server", "start_time", "end_time"]),
 
     Name() {
@@ -130,7 +181,7 @@ export default {
         if (status == this.status.status_valid) {
           return "取消";
         } else {
-          return "无法操作";
+          return "fuck";
         }
       };
     },
@@ -145,6 +196,49 @@ export default {
     },
   },
   methods: {
+    clickmenu(e) {
+      switch (e) {
+        case "cancel": {
+          console.log("取消 ", this.current_clicked_booking);
+          if (
+            this.current_clicked_booking.status == this.status.status_cancelled
+          ) {
+            this.$vux.toast.show({
+              text: "已取消",
+              type: "warn",
+            });
+          } else {
+            this.classroomId = this.current_clicked_booking.id;
+            this.showRefund = true;
+          }
+        }
+        case "showqr": {
+          console.log("显示二维码 ", this.current_clicked_booking);
+          this.qrsource = this.current_clicked_booking.id;
+
+          switch (this.current_clicked_booking.status) {
+            case this.status.status_valid: {
+              this.showqr = true;
+              break;
+            }
+            case this.status.status_invalid: {
+              this.$vux.toast.show({
+                text: "暂未通过审核",
+                type: "warn",
+              });
+              break;
+            }
+            case this.status.status_cancelled: {
+              this.$vux.toast.show({
+                text: "预订已经取消",
+                type: "warn",
+              });
+              break;
+            }
+          }
+        }
+      }
+    },
     refreshList() {
       this.$http
         .get(this.server + "/api/cr/my_booking", {
@@ -181,20 +275,11 @@ export default {
           console.log(err);
         });
     },
-
-    refund(e, status) {
-      if (status != this.status.status_valid) {
-        this.$vux.toast.show({
-          position: "middle",
-          type: "warn",
-          text: "无法操作",
-        });
-        return;
-      }
-      // prompt形式调用
-      this.showRefund = true;
-      this.classroomId = e;
+    clickButton(e) {
+      this.current_clicked_booking = e;
+      this.show_op_menu = true;
     },
+
     reserve() {
       this.$router.push("/selection_room");
     },
@@ -227,6 +312,9 @@ export default {
         })
         .catch((e) => {
           console.log(e);
+        })
+        .finally(() => {
+          this.refreshList();
         });
     },
     onShow() {},
@@ -236,8 +324,6 @@ export default {
 </script>
 
 <style scoped>
-/* @import "~vux/src/styles/1px.less"; */
-
 .card-flex {
   display: flex;
 }
@@ -279,5 +365,14 @@ export default {
 }
 .vux-form-preview {
   margin-top: 30px;
+}
+.qr {
+  display: flex;
+  flex-direction: column;
+}
+
+.qrcode {
+  margin: 0 auto;
+  padding-top: 50%;
 }
 </style>
